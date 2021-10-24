1e614c4dbba209bec3f522c400b78e9d60da8247
AYMEN DEV <68467119+steamdz@users.noreply.github.com>
2021-10-24 14:35:49 +0200

    commit intial

---
+++ main.py
@@ -0,0 +1,26 @@
+#AYMEN DEV 
+#القيام بالواجب برمجيا
+
+from turtle import *
+
+speed(1)
+setup(800,500)
+bgcolor("seagreen")
+
+penup()
+goto(-100,-100)
+pendown()
+
+color("red")
+begin_fill()
+circle(120)
+end_fill()
+
+title("Bangladesh - بنغلادش")
+mainloop()
+
+
+    
+
+
+
